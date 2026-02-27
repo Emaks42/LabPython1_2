04093ecf76b93d14a2f1554a53af15d05e56a8b8
Egor Maksimov <E@rand.ru>
2026-02-27 18:21:10 +0300

    Исправления кодстайла

--- src/generator_source.py
+++ src/generator_source.py
@@ -4,22 +4,23 @@ from constants import TASK_PURPOSES, TASK_DESTINATIONS
 
 
 class GeneratorSource:
-    state: object
+    seed: float
     chance_of_eof: int
 
-    def __init__(self, seed: int = random.randint(1, 100), amount_of_tasks: int = random.randint(10, 100)):
-        random.seed(seed)
-        self.state = random.getstate()
+    def __init__(self, seed: int | None = None, amount_of_tasks: int = random.randint(10, 100)):
+        if seed:
+            random.seed(seed)
+        self.seed = random.random()
         self.amount_of_tasks = amount_of_tasks
         self.returned_tasks_count = 0
 
     def get_task(self) -> str:
-        random.setstate(self.state)
-        destinantions_amount = random.randint(1, 2)
+        random.seed(self.seed)
+        destinations_amount = random.randint(1, 2)
         task = random.choice(TASK_PURPOSES)
-        for i in range(destinantions_amount):
+        for i in range(destinations_amount):
             task += " " + random.choice(TASK_DESTINATIONS)
-        self.state = random.getstate()
+        self.seed = random.random()
         task = str(int((sha256(task.encode())).hexdigest()[:3], 16)) + " - " + task
         self.returned_tasks_count += 1
         return task
